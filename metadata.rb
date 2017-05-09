278a746a38400d731429de591933edb69b40802e
Oswaldo Gimeno <ogimeno@example.es>
2017-05-09 18:55:14 +0000

    Initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name 'workstation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Oswaldo'
+maintainer_email 'ogimeno@example.com'
 license 'all_rights'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
 version '0.1.0'
-
+# test
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/workstation/issues' if respond_to?(:issues_url)
